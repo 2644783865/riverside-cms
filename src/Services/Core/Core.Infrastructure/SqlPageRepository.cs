843c268bba114834770e422e0529b2b96e0b3563
Michael Puddephat <mikepuddephat@hotmail.com>
2018-07-31 10:09:19 +0100

    Implement recursive listing of pages

--- src/Services/Core/Core.Infrastructure/SqlPageRepository.cs
+++ src/Services/Core/Core.Infrastructure/SqlPageRepository.cs
@@ -137,42 +137,209 @@ namespace Riverside.Cms.Services.Core.Infrastructure
             }
         }
 
+        private string GetPagesCteNonRecursiveSql()
+        {
+            return @"
+                WITH [Pages] AS
+                (
+                    SELECT TOP (@RowNumberUpperBound)
+                        ROW_NUMBER() OVER (ORDER BY
+			                CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 0 THEN cms.[Page].Created  END DESC,
+			                CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 0 THEN cms.[Page].Updated  END DESC,
+			                CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 0 THEN cms.[Page].Occurred END DESC,
+			                CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 0 THEN cms.[Page].Name     END DESC,
+			                CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 1 THEN cms.[Page].Created  END ASC,
+			                CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 1 THEN cms.[Page].Updated  END ASC,
+			                CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 1 THEN cms.[Page].Occurred END ASC,
+			                CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 1 THEN cms.[Page].Name     END ASC) AS RowNumber,
+		                cms.[Page].PageId
+                    FROM
+                        cms.[Page]
+	                INNER JOIN
+		                cms.[MasterPage]
+	                ON
+		                cms.[Page].TenantId = cms.MasterPage.TenantId AND
+		                cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
+                    WHERE
+		                cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = @ParentPageId OR (@ParentPageId IS NULL AND cms.[Page].ParentPageId IS NULL)) AND
+		                cms.MasterPage.PageType = @PageType
+                )
+            ";
+        }
+
+        private string GetPagesCteRecursiveSql()
+        {
+            return @"
+                DECLARE @Folders TABLE(
+                    PageId bigint NULL
+                )
+
+                -- Record all child folders under page that is passed to this stored procedure 
+
+                ;WITH [Folders] AS
+                (
+                    SELECT
+		                cms.[Page].PageId
+                    FROM
+                        cms.[Page]
+	                INNER JOIN
+		                cms.[MasterPage]
+	                ON
+		                cms.[Page].TenantId = cms.[MasterPage].TenantId AND
+		                cms.[Page].MasterPageId = cms.[MasterPage].MasterPageId
+                    WHERE
+		                cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = @ParentPageId OR (@ParentPageId IS NULL AND cms.[Page].ParentPageId IS NULL)) AND
+		                cms.[MasterPage].PageType = 0 /* PageType.Folder */
+	                UNION ALL
+	                SELECT
+		                ChildPage.PageId
+	                FROM
+		                cms.[Page] ChildPage
+	                INNER JOIN
+		                cms.[MasterPage] ChildMasterPage
+	                ON
+		                ChildPage.TenantId = ChildMasterPage.TenantId AND
+		                ChildPage.MasterPageId = ChildMasterPage.MasterPageId
+	                INNER JOIN
+		                [Folders]
+	                ON
+		                ChildPage.TenantId = @TenantId AND
+		                ChildPage.ParentPageId = [Folders].PageId
+	                WHERE
+		                ChildMasterPage.PageType = 0 /* PageType.Folder */
+                )
+
+                INSERT INTO
+	                @Folders (PageId)
+                SELECT
+	                [Folders].PageId
+                FROM
+	                [Folders]
+
+                -- Record the page that is passed to this stored procedure
+
+                IF (@ParentPageId IS NULL)
+	                INSERT INTO
+		                @Folders (PageId)
+	                VALUES
+		                (NULL)
+                ELSE
+	                INSERT INTO
+		                @Folders (PageId)
+	                SELECT
+		                cms.[Page].PageId
+	                FROM
+		                cms.[Page]
+	                INNER JOIN
+		                cms.[MasterPage]
+	                ON
+		                cms.[Page].TenantId = cms.[MasterPage].TenantId AND
+		                cms.[Page].MasterPageId = cms.[MasterPage].MasterPageId
+	                WHERE
+		                cms.[Page].TenantId = @TenantId AND
+		                cms.[Page].PageId = @ParentPageId AND
+		                cms.[MasterPage].PageType = 0 /* PageType.Folder */
+
+                -- Get pages into order so that we can extract the right pages according to the paging and sorting parameters
+
+                ;WITH [Pages] AS
+                (
+                    SELECT TOP (@RowNumberUpperBound)
+                        ROW_NUMBER() OVER (ORDER BY
+			                CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 0 THEN cms.[Page].Created  END DESC,
+			                CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 0 THEN cms.[Page].Updated  END DESC,
+			                CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 0 THEN cms.[Page].Occurred END DESC,
+			                CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 0 THEN cms.[Page].Name     END DESC,
+			                CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 1 THEN cms.[Page].Created  END ASC,
+			                CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 1 THEN cms.[Page].Updated  END ASC,
+			                CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 1 THEN cms.[Page].Occurred END ASC,
+			                CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 1 THEN cms.[Page].Name     END ASC) AS RowNumber,
+		                cms.[Page].PageId
+                    FROM
+                        @Folders [FoldersTable]
+	                INNER JOIN
+		                cms.[Page]
+	                ON
+		                cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = [FoldersTable].PageId OR ([FoldersTable].PageId IS NULL AND cms.[Page].ParentPageId IS NULL))
+	                INNER JOIN
+		                cms.[MasterPage]
+	                ON
+		                cms.[Page].TenantId = cms.MasterPage.TenantId AND
+		                cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
+                    WHERE
+		                cms.MasterPage.PageType	= @PageType
+                )
+            ";
+        }
+
+        private string GetPagesCteSql(bool recursive)
+        {
+            if (recursive)
+                return GetPagesCteRecursiveSql();
+            else
+                return GetPagesCteNonRecursiveSql();
+        }
+
+        private string GetTotalNonRecursiveSql()
+        {
+            return @"
+                SELECT
+	                COUNT(*) AS Total
+                FROM
+	                cms.[Page]
+                INNER JOIN
+	                cms.[MasterPage]
+                ON
+	                cms.[Page].TenantId = cms.MasterPage.TenantId AND
+	                cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
+                WHERE
+	                cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = @ParentPageId OR (@ParentPageId IS NULL AND cms.[Page].ParentPageId IS NULL)) AND
+	                cms.MasterPage.PageType = @PageType
+            ";
+        }
+
+        private string GetTotalRecursiveSql()
+        {
+            return @"
+                SELECT
+	                COUNT(*) AS Total
+                FROM
+	                cms.[Page]
+                INNER JOIN
+	                @Folders [FoldersTable]
+                ON
+	                cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = [FoldersTable].PageId OR ([FoldersTable].PageId IS NULL AND cms.[Page].ParentPageId IS NULL))
+                INNER JOIN
+	                cms.[MasterPage]
+                ON
+	                cms.[Page].TenantId = cms.MasterPage.TenantId AND
+	                cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
+                WHERE
+	                cms.MasterPage.PageType	= @PageType
+            ";
+        }
+
+        private string GetTotalSql(bool recursive)
+        {
+            if (recursive)
+                return GetTotalRecursiveSql();
+            else
+                return GetTotalNonRecursiveSql();
+        }
+
         public async Task<PageListResult> ListPages(long tenantId, long? parentPageId, bool recursive, PageType pageType, SortBy sortBy, bool sortAsc, int pageIndex, int pageSize)
         {
             using (SqlConnection connection = new SqlConnection(_options.Value.SqlConnectionString))
             {
                 connection.Open();
-                using (GridReader gr = await connection.QueryMultipleAsync(@"
+                using (GridReader gr = await connection.QueryMultipleAsync($@"
                     DECLARE @Pages TABLE ([RowNumber] [int] NOT NULL PRIMARY KEY CLUSTERED, [PageId] [bigint] NOT NULL)
                     DECLARE @RowNumberLowerBound int
                     DECLARE @RowNumberUpperBound int
                     SET @RowNumberLowerBound = @PageSize * @PageIndex
                     SET @RowNumberUpperBound = @RowNumberLowerBound + @PageSize + 1;
 
-                    WITH [Pages] AS
-                    (
-                        SELECT TOP (@RowNumberUpperBound)
-                            ROW_NUMBER() OVER (ORDER BY
-			                    CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 0 THEN cms.[Page].Created  END DESC,
-			                    CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 0 THEN cms.[Page].Updated  END DESC,
-			                    CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 0 THEN cms.[Page].Occurred END DESC,
-			                    CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 0 THEN cms.[Page].Name     END DESC,
-			                    CASE WHEN @SortBy = 0 /* PageSortBy.Created  */ AND @SortAsc = 1 THEN cms.[Page].Created  END ASC,
-			                    CASE WHEN @SortBy = 1 /* PageSortBy.Updated  */ AND @SortAsc = 1 THEN cms.[Page].Updated  END ASC,
-			                    CASE WHEN @SortBy = 2 /* PageSortBy.Occurred */ AND @SortAsc = 1 THEN cms.[Page].Occurred END ASC,
-			                    CASE WHEN @SortBy = 3 /* PageSortBy.Name     */ AND @SortAsc = 1 THEN cms.[Page].Name     END ASC) AS RowNumber,
-		                    cms.[Page].PageId
-                        FROM
-                            cms.[Page]
-	                    INNER JOIN
-		                    cms.[MasterPage]
-	                    ON
-		                    cms.[Page].TenantId = cms.MasterPage.TenantId AND
-		                    cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
-                        WHERE
-		                    cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = @ParentPageId OR (@ParentPageId IS NULL AND cms.[Page].ParentPageId IS NULL)) AND
-		                    cms.MasterPage.PageType = @PageType
-                    )
+                    {GetPagesCteSql(recursive)}
 
                     INSERT INTO
                         @Pages (RowNumber, PageId)
@@ -230,18 +397,7 @@ namespace Riverside.Cms.Services.Core.Infrastructure
                         cms.TagPage.PageId,
                         cms.Tag.Name
 
-                    SELECT
-	                    COUNT(*) AS Total
-                    FROM
-	                    cms.[Page]
-                    INNER JOIN
-	                    cms.[MasterPage]
-                    ON
-	                    cms.[Page].TenantId = cms.MasterPage.TenantId AND
-	                    cms.[Page].MasterPageId = cms.MasterPage.MasterPageId
-                    WHERE
-	                    cms.[Page].TenantId = @TenantId AND (cms.[Page].ParentPageId = @ParentPageId OR (@ParentPageId IS NULL AND cms.[Page].ParentPageId IS NULL)) AND
-	                    cms.MasterPage.PageType = @PageType
+                    {GetTotalSql(recursive)}
                 ",
                 new
                 {
