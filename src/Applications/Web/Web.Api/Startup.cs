c8860d54b8d919b66375e488edffd3af97d167aa
Michael Puddephat <mikepuddephat@hotmail.com>
2018-09-28 17:35:56 +0100

    Add Web.Api web application (will replace Cms.Api)

---
+++ src/Applications/Web/Web.Api/Startup.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Riverside.Cms.Services.Core.Domain;
+using Riverside.Cms.Services.Core.Infrastructure;
+using Riverside.Cms.Services.Element.Domain;
+using Riverside.Cms.Services.Element.Infrastructure;
+using Riverside.Cms.Services.Storage.Domain;
+using Riverside.Cms.Services.Storage.Infrastructure;
+using Riverside.Cms.Utilities.Net.Mail;
+using Riverside.Cms.Utilities.Text.Csv;
+using Riverside.Cms.Utilities.Text.Formatting;
+
+namespace Riverside.Cms.Applications.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private void ConfigureDependencyInjectionSharedServices(IServiceCollection services)
+        {
+            services.AddTransient<ICsvService, CsvService>();
+            services.AddTransient<IEmailService, SmtpEmailService>();
+            services.AddTransient<IStringUtilities, StringUtilities>();
+        }
+
+        private void ConfigureDependencyInjectionCoreServices(IServiceCollection services)
+        {
+            // Core domain services
+            services.AddTransient<IDomainService, DomainService>();
+            services.AddTransient<IForumService, ForumService>();
+            services.AddTransient<IMasterPageService, MasterPageService>();
+            services.AddTransient<IPageService, PageService>();
+            services.AddTransient<IPageViewService, PageViewService>();
+            services.AddTransient<ITagService, TagService>();
+            services.AddTransient<ITenantService, TenantService>();
+            services.AddTransient<IUserService, UserService>();
+
+            // Core infrastructure services
+            services.AddTransient<IDomainRepository, SqlDomainRepository>();
+            services.AddTransient<IForumRepository, SqlForumRepository>();
+            services.AddTransient<IMasterPageRepository, SqlMasterPageRepository>();
+            services.AddTransient<IPageRepository, SqlPageRepository>();
+            services.AddTransient<ITagRepository, SqlTagRepository>();
+            services.AddTransient<ITenantRepository, SqlTenantRepository>();
+            services.AddTransient<IUserRepository, SqlUserRepository>();
+        }
+
+        private void ConfigureDependencyInjectionElementServices(IServiceCollection services)
+        {
+            // Element domain services
+            services.AddTransient<IAlbumElementService, AlbumElementService>();
+            services.AddTransient<ICarouselElementService, CarouselElementService>();
+            services.AddTransient<ICodeSnippetElementService, CodeSnippetElementService>();
+            services.AddTransient<IFooterElementService, FooterElementService>();
+            services.AddTransient<IFormElementService, FormElementService>();
+            services.AddTransient<IHtmlElementService, HtmlElementService>();
+            services.AddTransient<ILatestThreadsElementService, LatestThreadsElementService>();
+            services.AddTransient<INavigationBarElementService, NavigationBarElementService>();
+            services.AddTransient<IPageHeaderElementService, PageHeaderElementService>();
+            services.AddTransient<IPageListElementService, PageListElementService>();
+            services.AddTransient<IShareElementService, ShareElementService>();
+            services.AddTransient<ISocialBarElementService, SocialBarElementService>();
+            services.AddTransient<ITableElementService, TableElementService>();
+            services.AddTransient<ITagCloudElementService, TagCloudElementService>();
+            services.AddTransient<ITestimonialElementService, TestimonialElementService>();
+
+            // Element infrastructure services
+            services.AddTransient<IElementRepository<AlbumElementSettings>, SqlAlbumElementRepository>();
+            services.AddTransient<IElementRepository<CarouselElementSettings>, SqlCarouselElementRepository>();
+            services.AddTransient<IElementRepository<CodeSnippetElementSettings>, SqlCodeSnippetElementRepository>();
+            services.AddTransient<IElementRepository<FooterElementSettings>, SqlFooterElementRepository>();
+            services.AddTransient<IElementRepository<FormElementSettings>, SqlFormElementRepository>();
+            services.AddTransient<IElementRepository<HtmlElementSettings>, SqlHtmlElementRepository>();
+            services.AddTransient<IElementRepository<LatestThreadsElementSettings>, SqlLatestThreadsElementRepository>();
+            services.AddTransient<IElementRepository<NavigationBarElementSettings>, SqlNavigationBarElementRepository>();
+            services.AddTransient<IElementRepository<PageHeaderElementSettings>, SqlPageHeaderElementRepository>();
+            services.AddTransient<IElementRepository<PageListElementSettings>, SqlPageListElementRepository>();
+            services.AddTransient<IElementRepository<ShareElementSettings>, SqlShareElementRepository>();
+            services.AddTransient<IElementRepository<SocialBarElementSettings>, SqlSocialBarElementRepository>();
+            services.AddTransient<IElementRepository<TableElementSettings>, SqlTableElementRepository>();
+            services.AddTransient<IElementRepository<TagCloudElementSettings>, SqlTagCloudElementRepository>();
+            services.AddTransient<IElementRepository<TestimonialElementSettings>, SqlTestimonialElementRepository>();
+        }
+
+        private void ConfigureDependencyInjectionStorageServices(IServiceCollection services)
+        {
+            // Storage domain services
+            services.AddTransient<IStorageService, StorageService>();
+
+            // Storage infrastructure services
+            services.AddTransient<IBlobService, AzureBlobService>();
+            services.AddTransient<IImageService, ImageService>();
+            services.AddTransient<IStorageRepository, SqlStorageRepository>();
+        }
+
+        private void ConfigureOptionServices(IServiceCollection services)
+        {
+            services.Configure<Riverside.Cms.Services.Core.Infrastructure.SqlOptions>(Configuration);
+            services.Configure<Riverside.Cms.Services.Element.Infrastructure.SqlOptions>(Configuration);
+            services.Configure<Riverside.Cms.Services.Storage.Infrastructure.SqlOptions>(Configuration);
+            services.Configure<AzureBlobOptions>(Configuration);
+            services.Configure<EmailOptions>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            ConfigureDependencyInjectionSharedServices(services);
+            ConfigureDependencyInjectionCoreServices(services);
+            ConfigureDependencyInjectionElementServices(services);
+            ConfigureDependencyInjectionStorageServices(services);
+            ConfigureOptionServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
