5af0e82b053070046157e406293363de262f58d8
Mike Puddephat <mikepuddephat@hotmail.com>
2019-04-26 09:10:58 +0100

    Implement image resize modes: simple, crop and maintain aspect

--- src/Utilities/Drawing/Drawing.ImageAnalysis/ImageAnalysisService.cs
+++ src/Utilities/Drawing/Drawing.ImageAnalysis/ImageAnalysisService.cs
@@ -1,8 +1,8 @@
-﻿using System.IO;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace Riverside.Cms.Utilities.Drawing.ImageAnalysis
 {
@@ -14,14 +14,8 @@ namespace Riverside.Cms.Utilities.Drawing.ImageAnalysis
             {
                 ImageMetadata metadata = null;
                 long position = stream.Position;
-                using (Image<Rgba32> image = Image.Load(stream))
-                {
-                    metadata = new ImageMetadata
-                    {
-                        Width = image.Width,
-                        Height = image.Height
-                    };
-                }
+                using (Image image = Image.FromStream(stream))
+                    metadata = new ImageMetadata { Width = image.Width, Height = image.Height };
                 stream.Position = position;
                 return metadata;
             }
@@ -31,26 +25,96 @@ namespace Riverside.Cms.Utilities.Drawing.ImageAnalysis
             }
         }
 
+        private Image ResizeDrawingImageSimple(Image sourceImage, ResizeOptions options)
+        {
+            Bitmap bitmap = new Bitmap(options.Width, options.Height, PixelFormat.Format24bppRgb);
+            bitmap.SetResolution(sourceImage.HorizontalResolution, sourceImage.VerticalResolution);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(sourceImage,
+                    /* Note: The -1, +2 prevents ringing artifacts on resized image */
+                    new Rectangle(-1, -1, options.Width + 2, options.Height + 2),
+                    new Rectangle(0, 0, sourceImage.Width, sourceImage.Height),
+                    GraphicsUnit.Pixel);
+            }
+            return bitmap;
+        }
+
+        private Image ResizeDrawingImageMaintainAspect(Image sourceImage, ResizeOptions options)
+        {
+            double aspectHeightRatio = ((double)options.Height) / ((double)sourceImage.Height);
+            double aspectWidthRatio = ((double)options.Width) / ((double)sourceImage.Width);
+            double aspectRatio = Math.Min(aspectHeightRatio, aspectWidthRatio);
+            int width = (int)(((double)sourceImage.Width) * aspectRatio);
+            int height = (int)(((double)sourceImage.Height) * aspectRatio);
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            bitmap.SetResolution(sourceImage.HorizontalResolution, sourceImage.VerticalResolution);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(sourceImage,
+                    /* Note: The -1, +2 prevents ringing artifacts on resized image */
+                    new Rectangle(-1, -1, width + 2, height + 2),
+                    new Rectangle(0, 0, sourceImage.Width, sourceImage.Height),
+                    GraphicsUnit.Pixel);
+            }
+            return bitmap;
+        }
+
+        private Image ResizeDrawingImageCrop(Image sourceImage, ResizeOptions options)
+        {
+            double cropHeightRatio = ((double)options.Height) / ((double)sourceImage.Height);
+            double cropWidthRatio = ((double)options.Width) / ((double)sourceImage.Width);
+            double cropRatio = Math.Max(cropHeightRatio, cropWidthRatio);
+            int unCroppedWidth = (int)(((double)sourceImage.Width) * cropRatio);
+            int unCroppedHeight = (int)(((double)sourceImage.Height) * cropRatio);
+            int left = -(unCroppedWidth - options.Width) / 2;
+            int top = -(unCroppedHeight - options.Height) / 2;
+            Bitmap bitmap = new Bitmap(options.Width, options.Height, PixelFormat.Format24bppRgb);
+            bitmap.SetResolution(sourceImage.HorizontalResolution, sourceImage.VerticalResolution);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(sourceImage,
+                    /* Note: The -1, +2 prevents ringing artifacts on resized image */
+                    new Rectangle(left - 1, top - 1, unCroppedWidth + 2, unCroppedHeight + 2),
+                    new Rectangle(0, 0, sourceImage.Width, sourceImage.Height),
+                    GraphicsUnit.Pixel);
+            }
+            return bitmap;
+        }
+
+        private Image ResizeDrawingImage(Image sourceImage, ResizeOptions options)
+        {
+            switch (options.Mode)
+            {
+                case ResizeMode.MaintainAspect:
+                    return ResizeDrawingImageMaintainAspect(sourceImage, options);
+
+                case ResizeMode.Crop:
+                    return ResizeDrawingImageCrop(sourceImage, options);
+
+                default: /* ResizeMode.Simple */
+                    return ResizeDrawingImageSimple(sourceImage, options);
+            }
+        }
+
         public Stream ResizeImage(Stream stream, ResizeOptions options)
         {
-            IImageFormat format;
+            MemoryStream destMemoryStream = null;
             long position = stream.Position;
-            MemoryStream ms = null;
-            using (Image<Rgba32> image = Image.Load(stream, out format))
+            using (Image image = Image.FromStream(stream))
             {
-                switch (options.Mode)
+                using (Image resizedImage = ResizeDrawingImage(image, options))
                 {
-                    case ResizeMode.Simple:
-                        image.Mutate(x => x.Resize(options.Width, options.Height));
-                        break;
+                    destMemoryStream = new MemoryStream();
+                    resizedImage.Save(destMemoryStream, image.RawFormat);
+                    destMemoryStream.Position = 0;
                 }
-
-                ms = new MemoryStream();
-                image.Save(ms, format);
-                ms.Position = 0;
             }
             stream.Position = position;
-            return ms;
+            return destMemoryStream;
         }
     }
 }
